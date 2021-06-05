8cb6b58314cc2eca928ec847624f36f455a44d65
EliptickBlock5 <nbjohnson@gmail.com>
2021-06-05 11:49:07 -0700

    mongo db connected and server running

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true },()=>{
+    console.log("you are connected to MongoDB")
+});
 
 app.listen(8800,()=>{
     console.log("nate your server is running")
