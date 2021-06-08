343c04128a5c428ed5466743c2ada3b7d37a1113
EliptickBlock5 <nbjohnson@gmail.com>
2021-06-08 15:05:41 -0700

    adding thought routes

--- routes/thought-route.js
+++ routes/thought-route.js
@@ -1,7 +1,38 @@
 const router = require("express").Router();
 
-router.get("/",(req,res)=>{
+const {
+    getAllTheThought,
+    getThoughtById,
+    createNewThought,
+    updateUserThought,
+    deleteUserThought,
+    addUserReaction,
+    removeUserReaction
+} = require('../controllers/thought-controller');
+
+router
+    .route('/')
+    .get(getAllTheThought)
+    .post(createNewThought)
+
+//by id
+router
+ .route('/:id')
+ .get(getThoughtById)
+ .put(updateUserThought)
+.delete(deleteUserThought)
+
+//routes for reaction
+
+router
+    .route("/:thoughtId/reactions")
+    .post(addUserReaction)
+router
+    .route('/:thoughtId/reactions/:reactionId')
+    .delete(removeUserReaction)
+
+/*router.get("/",(req,res)=>{
     res.send("you are now using the thought route")
-})
+})*/
 
 module.exports = router
