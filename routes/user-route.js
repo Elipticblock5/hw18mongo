07db95f3d9adf20d2eed3933d934e96b72d39628
EliptickBlock5 <nbjohnson@gmail.com>
2021-06-08 15:13:51 -0700

    user routes

--- routes/user-route.js
+++ routes/user-route.js
@@ -1,14 +1,41 @@
 //used askBCS ticket to assist with userControllers routing
 
-const userControllers = require("../controllers/user-controller");
+//const userControllers = require("../controllers/user-controller");
 
 const router = require("express").Router();
 
 
-router.route("/").post(userControllers.makeUser);
+const {
+    getAlltheUsers,
+    getUserID,
+    makeUser,
+    updateTheUser,
+    deleteTheUser,
+    addTheFriend,
+    deleteTheFriend
 
-router.get("/",(req,res)=>{
+}  = require("../controllers/user-controller");
+
+router
+    .route('/')
+    .get(getAlltheUsers)
+    .post(makeUser);
+
+//by id
+router  
+    .route('/:id')
+    .get(getUserID)
+    .put(updateTheUser)
+    .delete(deleteTheUser);
+
+//friends
+router
+    .route('/:userId/friends/:friendId')
+    .post(addTheFriend)
+    .delete(deleteTheFriend);
+
+/*router.get("/",(req,res)=>{
     res.send("you are now using the user route")
-})
+})*/
 
 module.exports = router
